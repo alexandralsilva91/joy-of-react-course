f279b5eed3c81ca24dd81a5878ee7975bba66bed
Alexandra Leite da Silva <alexandra@MacBook-Pro-de-Alexandra.local>
2025-03-08 23:33:18 +0000

    ideias básicas do react

---
+++ joy-of-react/src/main.jsx
@@ -0,0 +1,39 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+function Soma({ num1, num2 }) {
+    return <h2>{num1 + num2 + num3}</h2>
+}
+
+function Xana({ children }) {
+    return <h3>{children}</h3>
+}
+
+function HelloWorld({ name, children }) {
+    const date = new Date();
+
+    return (
+        <h1>Hello World {children} {name} {date.toISOString()}</h1>
+    );
+
+    // Depois de ser processado e "compilado" pelo vite o React vai ser comvertido em 
+    // algo parecido com isto:
+    //
+    // const h = document.createElement('h1');
+    // h.innerText = `hello world ${name} ${date.toISOString()}`
+    // document.body.append(h);
+    //  ^
+    //   \___ Este é o codigo javascript que vai ser gerado quando compilado
+    //        E faz exactamente o mesmo.
+
+}
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <HelloWorld name="Xana" />             {/*HelloWorld({ name: "Xana" }); */}
+        <Soma num1={1} num2={100} />           {/*Soma({ num1: 1, num2: 100 }); */}
+        <Xana>                                 {/*Xana({ children: "Eu sou a Xana" }); */}
+            Eu sou a Xana
+        </Xana>
+    </StrictMode>,
+);
